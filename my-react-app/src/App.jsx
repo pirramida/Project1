ca5808542171f6c9225bbb335aace8ce9182041b
pirramida <44705382+pirramida@users.noreply.github.com>
2025-02-14 15:07:31 +0300

    test

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -13,7 +13,7 @@ function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   return (
-    <>
+    <>фывфыв
     <Header setIsLoggedIn={setIsLoggedIn} />
     <BurgerMenu />
 
--- my-react-app/src/Pages/LibraryPage/Library.jsx
+++ my-react-app/src/Pages/LibraryPage/Library.jsx
@@ -1,9 +1,35 @@
-import React from 'react';
+import { TableBody } from '@mui/material';
+import React, {useEffect, useState, TableContainer, TableHead, TableRow, TableCell, Table} from 'react';
+
+const NamesTable = ["Название", "Автор", "Год издания", "Статус"];
 
 const Library = () => {
 
     return(
-        <></>
+        <>
+        <TableContainer>
+            <Table>
+                <TableHead>
+                    <TableRow >
+                    {NamesTable.map((name, indexName) => (
+                        <TableCell key={indexName}>
+                            {name}
+                        </TableCell>
+                    ))}
+                    </TableRow>
+                    
+                </TableHead>
+
+                <TableBody>
+                    <TableRow>
+                        <TableCell>
+
+                        </TableCell>
+                    </TableRow>
+                </TableBody>
+            </Table>
+        </TableContainer>
+        </>
     )
 };
 
